dabaeab5928024d0a06191546c4e5a71111f6fd8
WamalaH <Howardwamala@googlemail.com>
2023-09-15 22:07:02 +0100

    Added to project5

---
+++ Project 5/shell_scripting.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# name="John" 
+# echo $name
+
+
+
+# Example script to check if a number is positive, negative, or zero
+
+# read -p "Enter a number: " num
+
+# if [ $num -gt 0 ]; then
+ #   echo "The number is positive."
+# elif [ $num -lt 0 ]; then
+  #  echo "The number is negative."
+# else
+  #  echo "The number is zero."
+# fi
+
+#!/bin/bash
+
+# Example script to print numbers from 1 to 5 using a for loop
+
+# for (( i=1; i<=5; i++ ))
+# do
+  #  echo $i
+# done
+
+#!/bin/bash
+
+# Prompt the user for their name
+echo "Enter your name:"
+read name
+
+# Display a greeting with the entered name
+echo "Hello, $name! Nice to meet you."
+
+
+
